f7483290499f2df7ca2ed7e1e155135a8d42e233
EpicUsername12 <39063367+EpicUsername12@users.noreply.github.com>
2023-06-16 09:07:44 +0200

    - Update grpc_py to the latest protobuf versions - Remove pycache files

--- account/account_service_pb2_grpc.py
+++ account/account_service_pb2_grpc.py
@@ -2,6 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
+from . import get_nex_password_rpc_pb2 as get__nex__password__rpc__pb2
 from . import get_user_data_rpc_pb2 as get__user__data__rpc__pb2
 
 
@@ -19,12 +20,23 @@ class AccountStub(object):
                 request_serializer=get__user__data__rpc__pb2.GetUserDataRequest.SerializeToString,
                 response_deserializer=get__user__data__rpc__pb2.GetUserDataResponse.FromString,
                 )
+        self.GetNEXPassword = channel.unary_unary(
+                '/account.Account/GetNEXPassword',
+                request_serializer=get__nex__password__rpc__pb2.GetNEXPasswordRequest.SerializeToString,
+                response_deserializer=get__nex__password__rpc__pb2.GetNEXPasswordResponse.FromString,
+                )
 
 
 class AccountServicer(object):
     """Missing associated documentation comment in .proto file."""
 
     def GetUserData(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def GetNEXPassword(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
         context.set_details('Method not implemented!')
@@ -37,6 +49,11 @@ def add_AccountServicer_to_server(servicer, server):
                     servicer.GetUserData,
                     request_deserializer=get__user__data__rpc__pb2.GetUserDataRequest.FromString,
                     response_serializer=get__user__data__rpc__pb2.GetUserDataResponse.SerializeToString,
+            ),
+            'GetNEXPassword': grpc.unary_unary_rpc_method_handler(
+                    servicer.GetNEXPassword,
+                    request_deserializer=get__nex__password__rpc__pb2.GetNEXPasswordRequest.FromString,
+                    response_serializer=get__nex__password__rpc__pb2.GetNEXPasswordResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -62,5 +79,22 @@ class Account(object):
         return grpc.experimental.unary_unary(request, target, '/account.Account/GetUserData',
             get__user__data__rpc__pb2.GetUserDataRequest.SerializeToString,
             get__user__data__rpc__pb2.GetUserDataResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def GetNEXPassword(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/account.Account/GetNEXPassword',
+            get__nex__password__rpc__pb2.GetNEXPasswordRequest.SerializeToString,
+            get__nex__password__rpc__pb2.GetNEXPasswordResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
